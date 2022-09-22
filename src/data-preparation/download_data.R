862a752cdc79fbcf1b6f0fd3a9a23b3df88ce440
Roel Timmerman <roeltimmerman00@gmail.com>
2022-09-22 13:05:27 +0200

    The first source code for downloading the raw data.

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,8 +1,9 @@
-# Download dataset 1
-# dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file('https://rgreminger.github.io/files/dataset1.csv','./data/dataset1/dataset1.csv')
 
-# Download dataset 2
-# dir.create('./data/dataset2')  # Uncomment if need to create directory with R
-download.file('https://rgreminger.github.io/files/dataset2.csv','./data/dataset2/dataset2.csv')
+#.....Downloading the raw files......#
+
+# Download the listings data from march
+download.file('http://data.insideairbnb.com/united-states/la/new-orleans/2022-03-08/visualisations/listings.csv', 'data/dataset1/data_maart.csv')
+
+# Download the listings data from september
+download.file('http://data.insideairbnb.com/united-states/la/new-orleans/2021-12-07/data/listings.csv.gz', 'data/dataset1/data_september.csv')
 
