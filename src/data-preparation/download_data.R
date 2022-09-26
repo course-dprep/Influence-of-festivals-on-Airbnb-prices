c82469b08a0e650babda3c451a1a9866e6d37ac9
Gijsvu <112493029+Gijsvu@users.noreply.github.com>
2022-09-26 10:08:02 +0200

    Update download_data.R

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -7,3 +7,16 @@ download.file('http://data.insideairbnb.com/united-states/la/new-orleans/2022-03
 # Download the listings data from september
 download.file('http://data.insideairbnb.com/united-states/la/new-orleans/2021-12-07/data/listings.csv.gz', 'data/dataset1/data_september.csv')
 
+# Read csv for both sets
+library(readr)
+data_maart <- read.csv('data/dataset1/data_maart.csv')
+data_september <- read.csv('data/dataset1/data_september.csv')
+
+# Generate summary statistics
+View(data_maart)
+summary(data_maart)
+head(data_maart)
+
+View(data_september)
+summary(data_september)
+head(data_september)
