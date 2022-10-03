86347d48c7600cf6aba814b9a43ef3b1eb4a3c3d
Roel Timmerman <roeltimmerman00@gmail.com>
2022-10-03 12:04:17 +0200

    Changed the filepaths

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,33 +1,25 @@
-# Download the listings data from march
-download.file('http://data.insideairbnb.com/united-states/la/new-orleans/2022-03-08/visualisations/listings.csv', 'listings_data_maart.csv')
-
-# Download the calendar data from march
-download.file('http://data.insideairbnb.com/united-states/la/new-orleans/2022-03-08/data/calendar.csv.gz', 'calendar_data_maart.csv')
-
-# Download the listings data from december
-download.file('http://data.insideairbnb.com/united-states/la/new-orleans/2021-12-07/data/listings.csv.gz', 'listings_data_december.csv')
-
-# Download the calendar data from december
-download.file("http://data.insideairbnb.com/united-states/la/new-orleans/2021-12-07/data/calendar.csv.gz", "calendar_data_december.csv")
 
 
+#........DOWNLOAD DATA..........#
 
-# Read csv for both sets
+
+library(tidyverse)
 library(readr)
-data_maart <- read.csv('data_maart.csv')
-data_september <- read.csv('data_september.csv')
-calendar_data_maart <- read.csv('calendar_data_maart.csv')
 
-# Generate summary statistics
-View(data_maart)
-summary(data_maart)
-head(data_maart)
+dir.create('./data/listings')
+dir.create('./data/calendar')
 
-View(data_september)
-summary(data_september)
-head(data_september)
+# download listings data
+download.file('http://data.insideairbnb.com/united-states/la/new-orleans/2022-03-08/data/listings.csv.gz', 
+              './data/listings/listings_maart.csv')
+download.file('http://data.insideairbnb.com/united-states/la/new-orleans/2021-12-07/data/listings.csv.gz', 
+              './data/listings/listings_december.csv')
 
-View(calendar_data_maart)
-summary(calendar_data_maart)
-head(calendar_data_maart)
-tail(calendar_data_maart)
+# download calendar data
+download.file('http://data.insideairbnb.com/united-states/la/new-orleans/2022-03-08/data/calendar.csv.gz',
+              './data/calendar/calendar_maart.csv')
+download.file("http://data.insideairbnb.com/united-states/la/new-orleans/2021-12-07/data/calendar.csv.gz",
+              './data/calendar/calendar_december.csv')
+
+
+
--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -2,18 +2,28 @@ library(readr)
 library(tidyverse)
 
 # Load datasets into R 
-df_calendar_maart <- read_csv("calendar_data_maart.csv")
-df_calendar_december <- read_csv("calendar_data_december.csv")
-df_listings <- read_csv("listings_data_maart.csv")
+df_calendar_maart <- read_csv("./data/calendar/calendar_maart.csv")
+df_calendar_december <- read_csv("./data/calendar/calendar_december.csv")
+df_listings_maart <- read_csv("./data/listings/listings_maart.csv")
+df_listings_december <- read_csv("./data/listings/listings_december.csv")
 
 # Merge the calendar data sets together 
 df_calendar <- bind_rows(df_calendar_maart, df_calendar_december)
 
+# Merge the listings data sets together
+df_listings <- bind_rows(df_listings_maart, df_listings_december)
+
+# Remove duplicates in the id column
+df_listings <- df_listings[!duplicated(df_listings$id),]
+
 # Keep observations only from 1 March till May 31
-df_calendar_clean <- df_calendar %>% arrange(date) %>% filter(between(date, as.Date("2022-2-28"), as.Date("2022-5-31")))
+df_calendar_clean <- df_calendar %>% arrange(date) %>% filter(between(date, as.Date("2022-4-30"), as.Date("2022-5-31")))
 
 # Merge the full calendar dataset with the listings data set
 df_merged <- inner_join(df_calendar_clean, df_listings,  by = c("listing_id"="id"))
 
+# Keep 
+df_merged_clean <- df_merged[,c(1:5,8:10)]
+
 # Save merged data
-save(df_merged,file="./gen/data-preparation/temp/data_merged.RData")
+write_csv(df_merged_clean,file="./gen/data-preparation/temp/df_merged_clean.csv")
