22bfd105d7ab922510ab7c2d9ff388bb7423132b
Gijsvu <112493029+Gijsvu@users.noreply.github.com>
2022-10-05 16:48:27 +0200

    Update add_dummy.R
    Returns an error right now because the dates that we use are not compatible with the festival dates yet(due to our big file as discussed in the meeting), but it works on a different set where those data are available

--- src/data-preparation/add_dummy.R
+++ src/data-preparation/add_dummy.R
@@ -1,6 +1,20 @@
-# Create dummy for each festival separately and for any festival
+# Load data
+df_merged <- read_csv("./gen/data-preparation/temp/df_merged_clean.csv")
+View(df_merged)
 
-calendar_data_maart$Mardi_Gras_2023 <- if_else(calendar_data_maart$date_try >= as.Date('2023-02-23') & calendar_data_maart$date_try <= as.Date('2023-03-01'),TRUE,FALSE)
-calendar_data_maart$French_Quarter_2022 <- if_else(calendar_data_maart$date_try >= as.Date('2022-04-21') & calendar_data_maart$date_try <= as.Date('2022-04-24'),TRUE,FALSE)
-calendar_data_maart$Jazz_Heritage_2022 <- if_else(calendar_data_maart$date_try >= as.Date('2022-04-29') & calendar_data_maart$date_try <= as.Date('2022-05-08'),TRUE,FALSE)
-calendar_data_maart$festival_date <- if_else(calendar_data_maart$Mardi_Gras_2023 |calendar_data_maart$French_Quarter_2022 | calendar_data_maart$Jazz_Heritage_2022 == 1, TRUE, FALSE)
+# Create dummy variables for festival dates
+
+# Mardi Gras 23 February - 1 March 2022
+# French Quarter Festival 21 April - 24 April 2022
+# New Orleans Jazz & Heritage Festival 29 April - 8 May 2022
+
+
+df_merged$Mardi_Gras_test <- if_else(df_merged$date >= as.Date('2022-05-03') & df_merged$date <= as.Date('2022-05-08'),TRUE,FALSE)
+
+df_merged$Mardi_Gras_2022 <- if_else(df_merged$date >= as.Date('2022-02-23') & df_merged$date <= as.Date('2022-03-01'),TRUE,FALSE)
+df_merged$French_Quarter_2022 <- if_else(df_merged$date >= as.Date('2022-04-21') & df_merged$date <= as.Date('2022-04-24'),TRUE,FALSE)
+df_merged$Jazz_Heritage_2022 <- if_else(df_merged$date >= as.Date('2022-04-29') & df_merged$date <= as.Date('2022-05-08'),TRUE,FALSE)
+df_merged$festival_date <- if_else(df_merged$Mardi_Gras_2023 |df_merged$French_Quarter_2022 | df_merged$Jazz_Heritage_2022 == 1, TRUE, FALSE)
+
+# Save as df_dummy_added
+write_csv(df_merged,file="./gen/data-preparation/temp/df_dummy_added.csv")
