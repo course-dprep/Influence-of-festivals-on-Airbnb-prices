6ba2e8b20c79b138b90ee8e9282901df181f882f
Gijsvu <112493029+Gijsvu@users.noreply.github.com>
2022-10-05 20:33:32 +0200

    Update clean_data.R

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -4,6 +4,5 @@ df_merged <- read_csv("./gen/data-preparation/temp/df_merged_clean.csv")
 # set price as numeric
 df_merged$price_numeric <- as.numeric(parse_number(df_merged$price.x))
 
-
-
+# write csv
 write.csv(df_merged,file="./gen/data-preparation/temp/df_merged_clean.csv")
