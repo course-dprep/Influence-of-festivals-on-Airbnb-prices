197c5f0859a80c3f47d6153b12990c95ef2f477b
Gijsvu <112493029+Gijsvu@users.noreply.github.com>
2022-10-09 15:12:59 +0200

    Update analyze.R
    Converted to linear regression

--- src/analysis/analyze.R
+++ src/analysis/analyze.R
@@ -5,8 +5,7 @@ library(car)
 # Read input
 data_input <- read_csv("./gen/analysis/input/data_ready_for_analysis.csv")
 
-# Conduct Anova
-Anova_festivals <- lm(price_numeric ~ 
-                        Mardi_Gras_2022_factor+French_Quarter_2022_factor+Jazz_Heritage_2022_factor,data_input, 
-                      contrasts=list(Mardi_Gras_2022_factor=contr.sum, French_Quarter_2022_factor=contr.sum, Jazz_Heritage_2022_factor=contr.sum))
-Anova(anova_festivals, type=3)
+# Linear regression
+regression_festivals <- lm(price_numeric ~ 
+                        Mardi_Gras_2022_factor+French_Quarter_2022_factor+Jazz_Heritage_2022_factor+weekday_factor+host_neighbourhood_factor, data = data_input)
+summary(regression_festivals)
