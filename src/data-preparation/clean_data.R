dfa8655b9c77791e8c14b59ea97798ee16a45eff
Gijsvu <112493029+Gijsvu@users.noreply.github.com>
2022-10-09 15:09:29 +0200

    Update clean_data.R
    Added neighbourhood cleaning(filter NA's

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -1,8 +1,16 @@
+# Data preparation/cleaning
+
 # Load merged data 
 df_merged <- read_csv("./gen/data-preparation/temp/df_merged_clean.csv")
 
-# set price as numeric
+# Set price as numeric
 df_merged$price_numeric <- as.numeric(parse_number(df_merged$price.x))
 
-# write csv
+# Load dplyr
+library(dplyr)
+
+# Exclude NA's for neighborhoods
+df_merged <- df_merged %>% filter(!is.na(df_merged$host_neighbourhood))
+
+# Write csv
 write.csv(df_merged,file="./gen/data-preparation/temp/df_merged_clean.csv")
