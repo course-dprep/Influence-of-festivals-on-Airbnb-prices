cd273af728c0107989d4453df5261328c3399021
Gijsvu <112493029+Gijsvu@users.noreply.github.com>
2022-10-15 18:17:59 +0200

    Add files via upload
    install necessary packages

---
+++ src/data-preparation/Install_packages.R
@@ -0,0 +1,7 @@
+# install necessary packages
+install.packages("tidyverse", repos="https://mirrors.evoluso.com/CRAN/")
+install.packages("dplyr", repos="https://mirrors.evoluso.com/CRAN/")
+install.packages("readr", repos="https://mirrors.evoluso.com/CRAN/")
+install.packages("knitr", repos="https://mirrors.evoluso.com/CRAN/")
+install.packages("ggplot2", repos="https://mirrors.evoluso.com/CRAN/")
+install.packages("here", repos="https://mirrors.evoluso.com/CRAN/")
